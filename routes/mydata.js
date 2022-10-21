1121e1e18a5d07741e9d333897ea3080e19e9ca7
MahimaChowdaryG <gollamudimahimachowdary7@gmail.com>
2022-10-21 14:08:48 -0500

    updated user.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET aboutme page */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Mahima Chowdary Gollamudi' });
+  res.render('mydata', { title: 'Mahima Chowdary Gollamudi'});
 });
 
 module.exports = router;
